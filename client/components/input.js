64b52829779b58419fbb5fe8673122d93bde43f6
AnastasiyaSS <zamara.2901@gmail.com>
2020-11-03 14:19:05 +0700

    w12_input_added

--- client/components/input.js
+++ client/components/input.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 const InputUserId = (props) => {
   const [value, setValue] = useState('1')
